a274828a30906a62dcd1ae3bcef1ec700c4c93cb
Rokeer <me@colinzhang.com>
2018-11-14 18:48:10 -0500

    upgrade gensim

--- Unsupervised-Aspect-Extraction/w2vEmbReader.py
+++ Unsupervised-Aspect-Extraction/w2vEmbReader.py
@@ -17,10 +17,10 @@ class W2VEmbReader:
         logger.info('Loading embeddings from: ' + emb_path)
         self.embeddings = {}
         emb_matrix = []
-       
+
         model = gensim.models.Word2Vec.load(emb_path)
         self.emb_dim = emb_dim
-        for word in model.vocab:
+        for word in model.wv.vocab:
             self.embeddings[word] = list(model[word])
             emb_matrix.append(list(model[word]))
 
