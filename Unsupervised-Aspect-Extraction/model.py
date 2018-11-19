4fb6b3701567336662cfc08af883b1e19cf7a626
Rokeer <me@colinzhang.com>
2018-11-18 21:19:03 -0500

    new model file

--- Unsupervised-Aspect-Extraction/model.py
+++ Unsupervised-Aspect-Extraction/model.py
@@ -28,6 +28,62 @@ def create_model(args, maxlen, vocab):
 
     ##### Compute sentence representation #####
     e_w = word_emb(sentence_input)
+    #y_s = LSTM(args.emb_dim, return_sequences=False)(e_w)
+    y_s = Average()(e_w)
+    att_weights = Attention(name='att_weights')([e_w, y_s])
+    z_s = WeightedSum()([e_w, att_weights])
+
+    ##### Compute representations of negative instances #####
+    e_neg = word_emb(neg_input)
+    #z_n = TimeDistributed(LSTM(args.emb_dim, return_sequences=False))(e_neg)
+    z_n = Average()(e_neg)
+
+    ##### Reconstruction #####
+    p_t = Dense(args.aspect_size)(z_s)
+    p_t = Activation('softmax', name='p_t')(p_t)
+    r_s = WeightedAspectEmb(args.aspect_size, args.emb_dim, name='aspect_emb',
+            W_regularizer=ortho_reg)(p_t)
+
+    ##### Loss #####
+    loss = MaxMargin(name='max_margin')([z_s, z_n, r_s])
+    model = Model(inputs=[sentence_input, neg_input], outputs=loss)
+
+    ### Word embedding and aspect embedding initialization ######
+    if args.emb_path:
+        from w2vEmbReader import W2VEmbReader as EmbReader
+        emb_reader = EmbReader(args.emb_path, emb_dim=args.emb_dim)
+        logger.info('Initializing word embedding matrix')
+        # model.get_layer('word_emb').set_weights(emb_reader.get_emb_matrix_given_vocab(vocab, model.get_layer('word_emb').get_weights()[0]))
+        K.set_value(
+            model.get_layer('word_emb').embeddings,
+            emb_reader.get_emb_matrix_given_vocab(vocab, K.get_value(model.get_layer('word_emb').embeddings)))
+        logger.info('Initializing aspect embedding matrix as centroid of kmean clusters')
+        K.set_value(
+            model.get_layer('aspect_emb').W,
+            emb_reader.get_aspect_matrix(args.aspect_size))
+    return model
+
+
+
+def create_lstm_model(args, maxlen, vocab):
+
+    def ortho_reg(weight_matrix):
+        ### orthogonal regularization for aspect embedding matrix ###
+        w_n = weight_matrix / K.cast(K.epsilon() + K.sqrt(K.sum(K.square(weight_matrix), axis=-1, keepdims=True)), K.floatx())
+        reg = K.sum(K.square(K.dot(w_n, K.transpose(w_n)) - K.eye(w_n.shape[0].value)))
+        return args.ortho_reg*reg
+
+    vocab_size = len(vocab)
+
+    ##### Inputs #####
+    sentence_input = Input(shape=(maxlen,), dtype='int32', name='sentence_input')
+    neg_input = Input(shape=(args.neg_size, maxlen), dtype='int32', name='neg_input')
+
+    ##### Construct word embedding layer #####
+    word_emb = Embedding(vocab_size, args.emb_dim, mask_zero=True, name='word_emb')
+
+    ##### Compute sentence representation #####
+    e_w = word_emb(sentence_input)
     y_s = LSTM(args.emb_dim, return_sequences=False)(e_w)
     #y_s = Average()(e_w)
     att_weights = Attention(name='att_weights')([e_w, y_s])
@@ -62,10 +118,6 @@ def create_model(args, maxlen, vocab):
             model.get_layer('aspect_emb').W,
             emb_reader.get_aspect_matrix(args.aspect_size))
     return model
-
-
-
-    
 
 
 
